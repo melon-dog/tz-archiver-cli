20b8026f31604cf4a039f5abf0a108d05acb8544
melon-dog <JackTezos@proton.me>
2025-09-05 21:17:43 +0200

    Bump version to 1.0.2 in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -6,10 +6,10 @@ to the Internet Archive's Wayback Machine.
 
 Author: melon-dog
 License: MIT
-Version: 1.0.1
+Version: 1.0.2
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "melon-dog"
 __license__ = "MIT"
 __description__ = "Archive Tezos NFTs to Wayback Machine"
